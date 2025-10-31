b862174c99de6fd5f46684f1efce0710f69add17
abdulazizams <100981971+abdulazizams@users.noreply.github.com>
2025-10-31 23:38:06 +0300

    Update 1171_Pascals_Triangle_II.cs

--- LeetCode Explore/Array and String/1171_Pascals_Triangle_II.cs
+++ LeetCode Explore/Array and String/1171_Pascals_Triangle_II.cs
@@ -11,22 +11,28 @@ public class Pascals_Triangle_II {
 else if (rowIndex == 1){output.Add(1); return output;}
         }
         
-        //start by adding 1
+        //append 1 to list
         output.Add(1);
         //declare middle for readability
         int half;
 
+//to save space, use recursion
+//a row is recalculated rowIndex times
 for(int i = 2; i < rowIndex + 1; i++)
 {
+//middle point
     half = i/2 + i%2;
-    
+
+    //append i'th item
     output.Add(1);
-        
+
+        //each row is syymetric about middle
     for (int j = half; j > 0; j--)
     {
 output[j] += output[j - 1];
 }
-    
+
+    //mirror first half
     for (int j = i; j > half; j--)
     {
 output[j] = output[i-j];
